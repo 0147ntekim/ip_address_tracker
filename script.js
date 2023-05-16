a1fc2ab6ecb617c417c2109771064dd43c37dd8b
ghost147 <147ntekim@gmail.com>
2023-05-16 10:37:25 +0100

    'commit'

--- script.js
+++ script.js
@@ -2,49 +2,88 @@ const ipAddress = document.getElementById("ipAddress");
 const ipLocation = document.getElementById("location");
 const timezone = document.getElementById("timezone");
 const isp = document.getElementById("isp");
-const form = document.forms.ip;
-var map = L.map('map');
+const form = document.querySelector('form');
+const myMap = L.map('map');
 
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    ipData(e.target[0].value);
-    e.target[0].value = "";
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: `Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="https://www.github.com/0147ntekim">ghost devs</a>.`,
+}).addTo(myMap)
+
+const myIcon = L.icon({
+    iconUrl: './images/icon-location.svg',
+    iconSize: [40, 50],
+    iconAnchor: [20, 25],
 })
 
-const ipData = (address = "") => {
-    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_qoJBZrBxMVeTgrJkk7P3vZIbtmb0U&domain=${address}`)
-    .then(res => res.json())
-    .then(data => {
-        let lat = data.location.lat;
-        let lng = data.location.lng;
-        showIpData(data);
-        generateMap(lat,lng);
+let myMarker
+
+
+const mapDisplay = (lat, lng) => {
+    myMap.setView([lat, lng], 16)
+  
+    // We first check if there is no marker drawn on the map.
+    // If there is, we remove it
+    if (myMarker != null) myMarker.remove()
+  
+    myMarker = L.marker([lat, lng], { icon: myIcon })
+  
+    myMarker.addTo(myMap)
+}
+  
+
+const getData = (inputValue = '', searchType = 'IP') => {
+    const url =
+        searchType === 'IP'
+        ? `https://geo.ipify.org/api/v1?apiKey=at_e8YFs79ARdh1PfVH4s5rf6bJf99VW&ipAddress=${inputValue}`
+        : `https://geo.ipify.org/api/v1?apiKey=at_e8YFs79ARdh1PfVH4s5rf6bJf99VW&domain=${inputValue}`
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+        ipAddress.innerText = data.ip
+        ipLocation.innerText = `${data.location.region}, ${data.location.city}`
+        timezone.innerText = `UTC ${data.location.timezone}`
+        isp.innerText = data.isp
+        mapDisplay(data.location.lat, data.location.lng)
+    })
+    .catch((error) => {
+        ipAddress.innerText = '__'
+        ipLocation.innerText = '__'
+        timezone.innerText = '__'
+        isp.innerText = '__'
+
+        const myInput = form.ipInput
+        myInput.classList.add('error')
+
+        setTimeout(() => myInput.classList.remove('error'), 3000)
+        console.error(error)
     })
 }
 
-ipData();
+const regexIp = /^\b([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b(\.\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b){3}$/
+const regexDomain = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/
 
-const showIpData = (data) => {
-    ipAddress.innerHTML = data.ip;
-    ipLocation.innerHTML = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
-    timezone.innerHTML = `UTC ${data.location.timezone}`;
-    isp.innerHTML = data.isp;
-}
 
-const generateMap = (lat, lng) => {
-    let locationIcon = L.icon({
-        iconUrl: 'images/icon-location.svg',
-    
-        iconSize:     [30, 30], // size of the icon
-        iconAnchor:   [lat, lng], // point of the icon which will correspond to marker's location
-        popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
-    });
-    map.setView([lat, lng], 17);
-    
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    }).addTo(map);
-
-    L.marker([lat, lng], {icon: locationIcon}).addTo(map);
-}
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+  
+    const myInput = form.ipInput
+  
+    if (myInput.value.match(regexIp)) {
+      getData(myInput.value)
+    }
+  
+    if (myInput.value.match(regexDomain)) {
+      getData(myInput.value, (searchType = 'DOMAIN'))
+    }
+  
+    if (!myInput.value.match(regexDomain) && !myInput.value.match(regexIp)) {
+      myInput.classList.add('error')
+  
+      setTimeout(() => myInput.classList.remove('error'), 3000)
+    }
+  })
+  
+  // Load IP Address on the map on the initial page load
+  getData()
